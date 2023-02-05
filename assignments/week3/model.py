98cd01e922fc246f56bbcf1eab7a162775bd0846
Yung-Tang (Patrick) Chou <patrick.ytchou@gmail.com>
2023-02-04 20:05:23 -0500

    update first iteration of MNIST MLP

--- assignments/week3/model.py
+++ assignments/week3/model.py
@@ -1,8 +1,11 @@
 import torch
+import torch.nn as nn
 from typing import Callable
 
 
-class MLP:
+class MLP(torch.nn.Module):
+    """Neural Network to classify MNIST dataset."""
+
     def __init__(
         self,
         input_size: int,
@@ -19,12 +22,25 @@ class MLP:
             input_size: The dimension D of the input data.
             hidden_size: The number of neurons H in the hidden layer.
             num_classes: The number of classes C.
+            hidden_count: The number of hidden layers.
             activation: The activation function to use in the hidden layer.
             initializer: The initializer to use for the weights.
         """
-        ...
+        super(MLP, self).__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.num_classes = num_classes
+        self.hidden_count = hidden_count
+        self.actv = activation
+        self.initializer = initializer
 
-    def forward(self, x):
+        self.layers = nn.ModuleList()
+        self.layers += [nn.Linear(self.input_size, self.hidden_size)]
+        for i in range(hidden_count - 1):
+            self.layers += [nn.Linear(self.hidden_size, self.hidden_size)]
+        self.out = nn.Linear(self.hidden_size, self.num_classes)
+
+    def forward(self, x: torch.tensor) -> torch.tensor:
         """
         Forward pass of the network.
 
@@ -34,4 +50,11 @@ class MLP:
         Returns:
             The output of the network.
         """
-        ...
+        # flattening
+        x = x.reshape(x.shape[0], -1)
+
+        # train the model
+        x = self.actv(self.fc1(1))
+        output = self.fc2(x)
+
+        return output
