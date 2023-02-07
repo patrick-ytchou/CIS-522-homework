7261e05513a1c340c2410fac45329aa79e505105
Yung-Tang (Patrick) Chou <patrick.ytchou@gmail.com>
2023-02-07 12:10:10 -0500

    update model file

--- assignments/week3/model.py
+++ assignments/week3/model.py
@@ -36,7 +36,7 @@ class MLP(torch.nn.Module):
         self.initializer = initializer
 
         # Define dropout layer
-        self.dropout = nn.Dropout(0.1)
+        self.dropout = nn.Dropout(0.15)
 
         # Define feedforward neural network
         self.layers = nn.ModuleList()
