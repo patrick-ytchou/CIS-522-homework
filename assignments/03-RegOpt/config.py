533c81058c8962b98f4d785dba153712bc9ef71c
Yung-Tang (Patrick) Chou <patrick.ytchou@gmail.com>
2023-02-16 14:21:22 -0500

    update model file

--- assignments/03-RegOpt/config.py
+++ assignments/03-RegOpt/config.py
@@ -6,21 +6,27 @@ from torchvision.transforms import Compose, Normalize, ToTensor
 
 
 class CONFIG:
+    """Config file for the scheduler"""
 
     batch_size = 64
-    num_epochs = 2
+    num_epochs = 10
     initial_learning_rate = 0.001
     initial_weight_decay = 0
 
     lrs_kwargs = {
         # You can pass arguments to the learning rate scheduler
         # constructor here.
+        "last_epoch": -1,
+        "gamma": 0.81,
+        "c": 1.3,
     }
 
     optimizer_factory: Callable[
         [nn.Module], torch.optim.Optimizer
     ] = lambda model: torch.optim.Adam(
-        model.parameters(), lr=CONFIG.initial_learning_rate, weight_decay=CONFIG.initial_weight_decay,
+        model.parameters(),
+        lr=CONFIG.initial_learning_rate,
+        weight_decay=CONFIG.initial_weight_decay,
     )
 
     transforms = Compose(
--- assignments/03-RegOpt/scheduler.py
+++ assignments/03-RegOpt/scheduler.py
@@ -4,21 +4,53 @@ from torch.optim.lr_scheduler import _LRScheduler
 
 
 class CustomLRScheduler(_LRScheduler):
-    def __init__(self, optimizer, last_epoch=-1):
+    """Custom learning rate scheduler"""
+
+    def __init__(self, optimizer, last_epoch=-1, gamma=0.5, c=1.5):
         """
         Create a new scheduler.
+
+        Arguments:
+            optimizer: (torch.optim.Optimizer)
+
+            last_epoch: (int)
+
+            gamma : (float)
+
+            c : (float)
+
+
+        Returns:
+            None
+
 
         Note to students: You can change the arguments to this constructor,
         if you need to add new parameters.
 
+        TODO: Weibull
         """
         # ... Your Code Here ...
         super(CustomLRScheduler, self).__init__(optimizer, last_epoch)
+        self.gamma = gamma
+        self.optimizer = optimizer
+        self.last_epoch = last_epoch
+        self.c = c
 
     def get_lr(self) -> List[float]:
+        """_summary_
+
+        Returns:
+            List[float]: _description_
+        """
         # Note to students: You CANNOT change the arguments or return type of
         # this function (because it is called internally by Torch)
 
         # ... Your Code Here ...
         # Here's our dumb baseline implementation:
-        return [i for i in self.base_lrs]
+
+        if self.last_epoch == 0:
+            return [group["lr"] for group in self.optimizer.param_groups]
+
+        return [
+            group["lr"] * self.gamma**self.c for group in self.optimizer.param_groups
+        ]
