e42c7f41bf63a2fa896ae7883390c66fc2a4a20f
Yung-Tang (Patrick) Chou <patrick.ytchou@gmail.com>
2023-02-18 01:33:57 -0500

    update scheduler file

--- assignments/03-RegOpt/config.py
+++ assignments/03-RegOpt/config.py
@@ -2,34 +2,38 @@ from typing import Callable
 import torch
 import torch.optim
 import torch.nn as nn
-from torchvision.transforms import Compose, Normalize, ToTensor
+from torchvision.transforms import Compose, Normalize, ToTensor, Grayscale
 
 
 class CONFIG:
     """Config file for the scheduler"""
 
     batch_size = 64
-    num_epochs = 6
-    initial_learning_rate = 0.001
+    num_epochs = 10
+    initial_learning_rate = 0.00125
     initial_weight_decay = 0.001
 
     lrs_kwargs = {
         # You can pass arguments to the learning rate scheduler
         # constructor here.
         "last_epoch": -1,
-        "gamma": 0.01,
-        "c": 0.26,
-        "step_size": 2,
+        "gamma": 0.57,
+        "c": 0.66,
+        "step_size": 1,
     }
 
     optimizer_factory: Callable[
         [nn.Module], torch.optim.Optimizer
-    ] = lambda model: torch.optim.SGD(
+    ] = lambda model: torch.optim.Adam(
         model.parameters(),
         lr=CONFIG.initial_learning_rate,
         weight_decay=CONFIG.initial_weight_decay,
-        momentum=CONFIG.momentum,
-        nesterov=True,
     )
 
-    transforms = Compose([ToTensor(), Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    transforms = Compose(
+        [
+            ToTensor(),
+            Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+            Grayscale(num_output_channels=3),
+        ]
+    )
