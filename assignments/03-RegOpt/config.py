630e6c29b8064b72b4cb763ed392c45fd5223b93
Yung-Tang (Patrick) Chou <patrick.ytchou@gmail.com>
2023-02-18 01:04:57 -0500

    update scheduler file

--- assignments/03-RegOpt/config.py
+++ assignments/03-RegOpt/config.py
@@ -3,7 +3,6 @@ import torch
 import torch.optim
 import torch.nn as nn
 from torchvision.transforms import Compose, Normalize, ToTensor
-import numpy as np
 
 
 class CONFIG:
@@ -11,24 +10,26 @@ class CONFIG:
 
     batch_size = 64
     num_epochs = 6
-    initial_learning_rate = 0.00125
-    initial_weight_decay = 0
+    initial_learning_rate = 0.001
+    initial_weight_decay = 0.001
 
     lrs_kwargs = {
         # You can pass arguments to the learning rate scheduler
         # constructor here.
         "last_epoch": -1,
         "gamma": 0.01,
-        "c": 0.5,
-        "step_size": 1,
+        "c": 0.26,
+        "step_size": 2,
     }
 
     optimizer_factory: Callable[
         [nn.Module], torch.optim.Optimizer
-    ] = lambda model: torch.optim.Adam(
+    ] = lambda model: torch.optim.SGD(
         model.parameters(),
         lr=CONFIG.initial_learning_rate,
         weight_decay=CONFIG.initial_weight_decay,
+        momentum=CONFIG.momentum,
+        nesterov=True,
     )
 
     transforms = Compose([ToTensor(), Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
