76faa81d32ea43cf415ba86a2c81af173eb73e99
Yung-Tang (Patrick) Chou <patrick.ytchou@gmail.com>
2023-02-18 00:37:39 -0500

    update scheduler file

--- assignments/03-RegOpt/config.py
+++ assignments/03-RegOpt/config.py
@@ -3,23 +3,23 @@ import torch
 import torch.optim
 import torch.nn as nn
 from torchvision.transforms import Compose, Normalize, ToTensor
-
+import numpy as np
 
 class CONFIG:
     """Config file for the scheduler"""
 
     batch_size = 64
-    num_epochs = 8
+    num_epochs = 6
     initial_learning_rate = 0.001
-    initial_weight_decay = 0.001
+    initial_weight_decay = 0
 
     lrs_kwargs = {
         # You can pass arguments to the learning rate scheduler
         # constructor here.
         "last_epoch": -1,
-        "gamma": 0.015,
-        "c": 0.71,
-        "step_size": 1,
+        "gamma": 0.01,
+        "c": 0.5,
+        "step_size": 2,
     }
 
     optimizer_factory: Callable[
