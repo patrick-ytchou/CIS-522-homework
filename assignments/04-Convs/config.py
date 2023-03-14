8d5b95d42b28fd4aebd62046f1f4836e9b432f83
Yung-Tang (Patrick) Chou <patrick.ytchou@gmail.com>
2023-03-14 16:45:42 -0400

    update model file

--- assignments/04-Convs/config.py
+++ assignments/04-Convs/config.py
@@ -6,12 +6,12 @@ from torchvision.transforms import Compose, ToTensor, Normalize
 
 
 class CONFIG:
-    batch_size = 64
+    batch_size = 128
     num_epochs = 4
 
     optimizer_factory: Callable[
         [nn.Module], torch.optim.Optimizer
     ] = lambda model: torch.optim.Adam(model.parameters(), lr=1e-3)
 
-    # transforms = Compose([ToTensor(), Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-    transforms = Compose([ToTensor()])
+    transforms = Compose([ToTensor(), Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    # transforms = Compose([ToTensor()])
--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -10,41 +10,38 @@ class Model(nn.Module):
 
     def __init__(self, num_channels: int, num_classes: int) -> None:
         super(Model, self).__init__()
-        self.out_channels1 = 5
-        self.out_channels2 = 7
+        self.out_channels1 = 7
+        self.out_channels2 = 5
 
         self.conv1 = nn.Conv2d(
             num_channels,
             self.out_channels1,
-            kernel_size=3,
-            stride=2,
+            kernel_size=2,
+            stride=1,
             padding=1,
             bias=False,
         )
 
-        # self.conv2 = nn.Conv2d(
-        #     self.out_channels1,
-        #     self.out_channels2,
-        #     kernel_size=3,
-        #     stride=1,
-        #     padding=1,
-        #     bias=False,
-        # )
+        self.conv2 = nn.Conv2d(
+            self.out_channels1,
+            self.out_channels2,
+            kernel_size=2,
+            stride=1,
+            padding=1,
+            bias=False,
+        )
 
         self.bn1 = nn.BatchNorm2d(self.out_channels1)
-        # self.bn2 = nn.BatchNorm2d(self.out_channels2)
+        self.bn2 = nn.BatchNorm2d(self.out_channels2)
 
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
 
-        self.fc1 = nn.Linear(self.out_channels1 * 8 * 8, 16)
-        self.fc2 = nn.Linear(16, num_classes)
-
-        # self.fc1 = nn.Linear(self.out_channels2 * 8 * 8, num_classes)
+        self.fc1 = nn.Linear(self.out_channels2 * 8 * 8, num_classes)
 
         nn.init.xavier_uniform_(self.conv1.weight)
-        # nn.init.xavier_uniform_(self.conv2.weight)
+        nn.init.xavier_uniform_(self.conv2.weight)
         nn.init.xavier_uniform_(self.fc1.weight)
-        nn.init.xavier_uniform_(self.fc2.weight)
+        # nn.init.xavier_uniform_(self.fc2.weight)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """Define training sequence for the CNN.
@@ -60,14 +57,14 @@ class Model(nn.Module):
         x = F.relu(x)
         x = self.pool(x)
 
-        # x = self.conv2(x)
-        # x = self.bn2(x)
-        # x = nn.functional.relu(x)
-        # x = self.pool(x)
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = nn.functional.relu(x)
+        x = self.pool(x)
 
-        x = x.view(-1, self.out_channels1 * 8 * 8)
+        x = x.view(-1, self.out_channels2 * 8 * 8)
 
-        x = F.relu(self.fc1(x))
-        x = self.fc2(x)
+        # x = F.relu(self.fc1(x))
+        x = self.fc1(x)
 
         return x
