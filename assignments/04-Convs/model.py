3dea743f86d5b6dfe4142b6bedff4bb0dda6fffd
Yung-Tang (Patrick) Chou <patrick.ytchou@gmail.com>
2023-03-16 13:22:18 -0400

    update model file

--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -10,8 +10,8 @@ class Model(nn.Module):
 
     def __init__(self, num_channels: int, num_classes: int) -> None:
         super(Model, self).__init__()
-        self.out_channels1 = 16
-        self.out_channels2 = 32
+        self.out_channels1 = 12
+        self.out_channels2 = 24
 
         self.conv1 = nn.Conv2d(
             num_channels,
